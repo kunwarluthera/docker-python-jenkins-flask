894a87d1c3278bd71ff80831bd612a4eb2ca606b
Kunwar Luthera <kunwarluthera@Kunwars-MBP.frontierlocal.net>
2019-03-11 06:37:22 -0500

    Updated .travis.yml and app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
 SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-#client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY, region_name='us-east-1')
 
 #print(client)
 app = Flask(__name__)
@@ -14,11 +14,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World, this is my first python API inside docker"
 
-#@app.route("/list-buckets")
-#def buckets():
-    #response = client.list_buckets()
-    #print(response)
-    #return str(response)
+@app.route("/list-buckets")
+def buckets():
+    response = client.list_buckets()
+    print(response)
+    return str(response)
 
 #print(str(buckets()))
 if __name__ == "__main__":
