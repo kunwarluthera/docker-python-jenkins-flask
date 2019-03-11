52bcb10e06ed6e90dcf710e8448a3eb679e26152
Kunwar Luthera <kunwarluthera@Kunwars-MBP.frontierlocal.net>
2019-03-11 06:53:17 -0500

    removed elastic folder

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 import boto3
 import os
 
-ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
-SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+#SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY, region_name='us-east-1')
+client = boto3.client('s3',aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name='us-east-1')
 
 #print(client)
 app = Flask(__name__)
