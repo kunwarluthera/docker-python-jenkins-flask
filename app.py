2b1e0b4929537482e484bc1ee1f8138c0cd6dec9
Kunwar Luthera <kunwarluthera@Kunwars-MBP.frontierlocal.net>
2019-03-11 07:00:34 -0500

    updated app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 #ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
 #SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-client = boto3.client('s3',aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name='us-east-1')
+#client = boto3.client('s3',aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name='us-east-1')
 
 #print(client)
 app = Flask(__name__)
@@ -16,8 +16,9 @@ def hello():
 
 @app.route("/list-buckets")
 def buckets():
-    response = client.list_buckets()
-    print(response)
+    response = "Inside List buckets"
+    #response = client.list_buckets()
+    #print(response)
     return str(response)
 
 #print(str(buckets()))
